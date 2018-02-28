0d271e40c803aa003881be27e372f368c53f4a0f
tyroneopinary <tyrone@opinary.com>
2018-02-28 14:57:11 +0100

    cleanup

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,38 @@
 import React, { Component } from 'react'
 import Form from './Form'
 import Preview from '../components/Preview'
-
 import '../styles/App.css'
-
 
 class App extends Component {
 
-  state ={
+  state = {
     colorPrimary: {
-      r: '16',
-      g: '1',
-      b: '1',
-      a: '1',
+      r: 16, g: 1, b: 1, a: 1
     },
     colorOpinion: {
-      r: '255',
-      g: '102',
-      b: '35',
-      a: '1',
+      r: 255, g: 102, b: 35, a: 1
     },
     colorHandlerPrimary: {
-      r: '245',
-      g: '0',
-      b: '12',
-      a: '1',
+      r: 245, g: 0, b: 12, a: 1
     },
     colorHandlerSecondary: {
-      r: '255',
-      g: '253',
-      b: '187',
-      a: '1',
+      r: 255, g: 253, b: 187, a: 1
     },
     colorComponent: {
-      r: '216',
-      g: '216',
-      b: '216',
-      a: '1',
+      r: 216, g: 216, b: 216, a: 1
     }
+  }
+
+  formToJSON = () => {
+    const obj = {
+      primary: this.state.colorPrimary,
+      opinion: this.state.colorOpinion,
+      handlerPrimary: this.state.colorHandlerPrimary,
+      handlerSecondary: this.state.colorHandlerSecondary,
+      component: this.state.colorComponent
+    }
+
+    console.log(JSON.stringify(obj))
   }
 
   render() {
@@ -75,6 +70,8 @@ class App extends Component {
           colorHandlerSecondary={colorHandlerSecondary}
           colorComponent={colorComponent}
         />
+
+        <button onClick={this.formToJSON}>Save</button>
 
       </div>
     )
